c8e0e3fe5b1ca441271f2fbb709bb8402d6c574b
yedam <dodgo12@naver.com>
2023-01-05 12:26:46 +0900

    0105/1226 개발환경구축완료

---
+++ own/src/main/java/co/prjt/own/ownhome/service/EmpVO.java
@@ -0,0 +1,15 @@
+package co.prjt.own.ownhome.service;
+
+import lombok.Data;
+
+@Data
+public class EmpVO {
+	String employeeId;
+	String lastName;
+	String job;
+	String mgr;
+	String hiredate;
+	String sal;
+	String comm;
+	String deptno;
+}
---
+++ own/src/test/java/co/prjt/own/OwnApplicationTests.java
@@ -0,0 +1,13 @@
+package co.prjt.own;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
