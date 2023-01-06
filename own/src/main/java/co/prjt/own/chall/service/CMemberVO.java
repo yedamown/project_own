dfdb7c54dd4dfece2d65bceef7570aa9a0177448
sujin <hsujin0819@gmail.com>
2023-01-06 10:29:32 +0900

    0106 am 10:29 sujin

--- own/src/main/java/co/prjt/own/chall/service/CMemberVO.java
+++ own/src/main/java/co/prjt/own/chall/service/CMemberVO.java
@@ -1,5 +1,11 @@
 package co.prjt.own.chall.service;
 
 public class CMemberVO {
-
+	String userId;
+	String memNickname;
+	int memAmt;
+	int memAcc;
+	String memBank;
+	String memIntro;
+	String memAccname;
 }
--- own/src/main/java/co/prjt/own/chall/service/ChallVO.java
+++ own/src/main/java/co/prjt/own/chall/service/ChallVO.java
@@ -1,5 +1,24 @@
 package co.prjt.own.chall.service;
 
-public class ChallVO {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class ChallVO {
+	String challNo;
+	String challCategory;
+	String challTag;
+	String challTitle;
+	int challDuration;
+	int challFreq;
+	Date challStartdate;
+	int nowMember;
+	int maxMember;
+	String challIntro;
+	String vldMethod;
+	String challPwd;
+	String challStatus;
+	String challLeader;
+	String restPrice;
 }
