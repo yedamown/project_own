ee9f02960450daa26d3302faf5a1a5c71f51eb92
yedam <dodgo12@naver.com>
2023-01-06 14:02:19 +0900

    1402 회원가입추가

--- own/src/main/java/co/prjt/own/ownhome/web/OwnhomeController.java
+++ own/src/main/java/co/prjt/own/ownhome/web/OwnhomeController.java
@@ -3,28 +3,42 @@ package co.prjt.own.ownhome.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import co.prjt.own.ownhome.mapper.OwnhomeMapper;
+import co.prjt.own.ownhome.service.OwnUserVO;
 
 
 @Controller
 public class OwnhomeController {
 
 	@Autowired
-	OwnhomeMapper empMapper;
+	OwnhomeMapper ownMapper;
 	 //수정테스트
 	 //통신 방식이 상관없다면 Request~로 퉁치기. 아니라면 get.. post..정해주기
-	@RequestMapping(value = "/own", method = RequestMethod.GET)
+	@RequestMapping(value = "/own/home", method = RequestMethod.GET)
 	public String ownHome(Model model) { //오운홈으로 가는 페이지이동
-		model.addAttribute("empList", empMapper.getEmpList(null));
+		model.addAttribute("empList", ownMapper.getEmpList(null));
 		return "content/own/ownhome"; 
 	}
 	
-	@RequestMapping(value = "/ownlogin", method = RequestMethod.GET)
+	@RequestMapping(value = "/own/login", method = RequestMethod.GET)
 	public String ownLogin(Model model) { // 오운로그인으로..
 		return "content/own/ownlogin"; 
 	}
 	
-}
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test(Model model) { //오운홈으로 가는 페이지이동
+		return "fragments/header"; 
+	}
+	//등록
+	@PostMapping("/own/userInto")
+		public OwnUserVO insert(OwnUserVO vo) {
+		ownMapper.insertUser(vo);
+		return vo;
+//	}
+	}
+	}
