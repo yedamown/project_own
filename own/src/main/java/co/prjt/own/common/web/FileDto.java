e509a4d1bd7f8395e9e0c9614dffa7070f156c67
yedam <dodgo12@naver.com>
2023-01-09 15:23:38 +0900

    마스터

--- own/src/main/java/co/prjt/own/common/web/FileDto.java
+++ own/src/main/java/co/prjt/own/common/web/FileDto.java
@@ -19,16 +19,3 @@ public class FileDto {
 		}
 }
 
-      private String uuid;
-      private String fileName;
-      private String contentType;
-      
-      public FileDto() {}
-      
-      public FileDto(String uuid, String fileName, String contentType) {
-         this.uuid = uuid;
-         this.fileName = fileName;
-         this.contentType = contentType;
-      }
-}
-
