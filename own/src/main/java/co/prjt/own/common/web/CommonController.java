08a4f0e131a0c823ac94d3c874e5eba1f3e4f343
Mihyun <rnjsalgus211@naver.com>
2023-01-09 15:18:38 +0900

    중간커밋

--- own/src/main/java/co/prjt/own/common/web/CommonController.java
+++ own/src/main/java/co/prjt/own/common/web/CommonController.java
@@ -1,26 +1,89 @@
 package co.prjt.own.common.web;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
-import co.prjt.own.common.mapper.CommonMapper;
 import co.prjt.own.common.service.CommonService;
 import co.prjt.own.common.service.ExersubVO;
 
 
 @Controller
 public class CommonController {
-		
-	@Autowired
-	CommonService commonService;
-	
-	@GetMapping("/common/exersub")
-	@ResponseBody//데이터를 반환할때는 무조건 리스폰스바디 넣기
-	public List<ExersubVO> getListexersub(ExersubVO vo){
-		return commonService.getListExersub();
-	}
+
+   
+   @Autowired
+   CommonService commonService;
+
+   @Value("${spring.servlet.multipart.location}")
+   String filePath;
+   
+   // 운동종류 가져오기
+   @GetMapping("/common/exersub")
+   @ResponseBody//데이터를 반환할때는 무조건 리스폰스바디 넣기
+   public List<ExersubVO> getListexersub(ExersubVO vo){
+      return commonService.getListExersub();
+   }
+   
+   @PostMapping("/upload")
+   public String upload(@RequestParam MultipartFile[] uploadfile, Model model) throws IllegalStateException, IOException{
+      
+      List<FileDto> list = new ArrayList<>();
+      for(MultipartFile file : uploadfile) {
+         if(!file.isEmpty()) {
+            FileDto dto = new FileDto(UUID.randomUUID().toString(),
+                                 file.getOriginalFilename(),
+                                 file.getContentType());
+            list.add(dto);
+            
+            File newFileName = new File(dto.getUuid()+"_"+dto.getFileName());
+
+            file.transferTo(newFileName);
+         }
+      }
+      model.addAttribute("files",list);
+      return "content/own/result";
+   }
+   
+   @GetMapping("/download")
+   public ResponseEntity<Resource> download(@ModelAttribute FileDto dto) throws IOException{
+      Path path = Paths.get(filePath + "/" + dto.getUuid() + "_" + dto.getFileName());
+      
+      String contentType = Files.probeContentType(path);
+      
+      HttpHeaders headers = new HttpHeaders();
+      headers.setContentDisposition(ContentDisposition.builder("attachment")
+            .filename(dto.getFileName(),
+                  StandardCharsets.UTF_8).build());
+      headers.add(HttpHeaders.CONTENT_TYPE, contentType);
+      
+      Resource resource = new InputStreamResource(Files.newInputStream(path)); 
+      
+      return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+   }
+   
 }
+
--- own/src/main/java/co/prjt/own/sns/web/SnsController.java
+++ own/src/main/java/co/prjt/own/sns/web/SnsController.java
@@ -8,9 +8,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import co.prjt.own.sns.mapper.SAccountMapper;
 import co.prjt.own.sns.mapper.SBoardMapper;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Controller
+@RequestMapping("/own")
 public class SnsController {
+	
 	@Autowired
 	SAccountMapper snsMapper;
 	
@@ -19,19 +23,28 @@ public class SnsController {
 	 //통신 방식이 상관없다면 Request~로 퉁치기. 아니라면 get.. post..정해주기
 	
 	//1. sns홈으로 이동
-	@RequestMapping(value = "/own/sns", method = RequestMethod.GET)
+	@RequestMapping(value = "/sns", method = RequestMethod.GET)
 	public String getSnsUserList(Model model) {
 		model.addAttribute("snsHome", snsMapper.getSnsUserList(null));
 		return "content/sns/snsHome"; 
 	}
 	
 	//2. 개인피드
-	@RequestMapping(value = "/own/snsFeed", method = RequestMethod.GET)
+	
+	@RequestMapping(value = "/snsFeed", method = RequestMethod.GET)
 	public String getSnsUser(Model model) {
 		model.addAttribute("snsFeed", boardMapper.getSnsBoardList(null));
 		return "content/sns/snsFeed"; 
 	}
 	
+	@RequestMapping(value = "/snsFeed2", method = RequestMethod.GET)
+	public String getSnsUser2(Model model) {
+		model.addAttribute("snsFeed2", boardMapper.getSnsBoardList(null));
+		return "content/sns/snsFeed2"; 
+	}
+	
+	
+	//3. 파일 업로드
 	
 	
 	
