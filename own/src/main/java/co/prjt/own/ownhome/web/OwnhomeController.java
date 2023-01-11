869be3f3323052211aa135588d302d59b5255792
권미현 <@175.113.26.192>
2023-01-11 21:17:10 +0900

    asdasd1

--- own/src/main/java/co/prjt/own/ownhome/web/OwnhomeController.java
+++ own/src/main/java/co/prjt/own/ownhome/web/OwnhomeController.java
@@ -57,7 +57,7 @@ public class OwnhomeController {
 		if (chk.getUserPasswd().equals(vo.getUserPasswd())) {
 			HttpSession session = request.getSession();
 			session.setAttribute("loginUser", chk);
-//		session.setAttribute("snsNickname",ownMapper.snsLogin(vo.getUserId()));
+			session.setAttribute("snsNickname",ownMapper.snsLogin(vo.getUserId()));
 			return 1;
 		} else
 			return 0;
