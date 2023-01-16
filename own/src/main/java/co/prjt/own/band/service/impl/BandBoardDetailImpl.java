5b3b6c4041d31599315b7d94c60eb4e5b63429e7
home <nalza1111@naver.com>
2023-01-17 01:57:45 +0900

    hjj 230116

--- own/src/main/java/co/prjt/own/band/service/impl/BandBoardDetailImpl.java
+++ own/src/main/java/co/prjt/own/band/service/impl/BandBoardDetailImpl.java
@@ -1,10 +1,13 @@
 package co.prjt.own.band.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import co.prjt.own.band.mapper.BandBoardDetailMapper;
 import co.prjt.own.band.service.BandBoardDetailService;
+import co.prjt.own.band.service.BandBoardDetailVO;
 
 
 @Service
@@ -14,5 +17,10 @@ public class BandBoardDetailImpl implements BandBoardDetailService{
 	@Override
 	public int countBandBoard(String bandNo) {
 		return bandBoardDetailMapper.countBandBoard(bandNo);
+	}
+
+	@Override
+	public List<BandBoardDetailVO> getFiveBoard(BandBoardDetailVO vo) {
+		return bandBoardDetailMapper.getFiveBoard(vo);
 	}
 }
