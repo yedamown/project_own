fdc31b28d019cf8763e535cc066b673b0c95dcca
nammiju <nmj_1025@naver.com>
2023-01-30 17:48:14 +0900

    멤버 리스트 페이징, 검색, 정렬만 완료...

--- own/src/main/java/co/prjt/own/band/web/BandOptionController.java
+++ own/src/main/java/co/prjt/own/band/web/BandOptionController.java
@@ -81,29 +81,30 @@ public class BandOptionController {
 	// 멤버 관리 페이지로 이동
 	@GetMapping("/bandGroup/bandMemberManage")
 	public String bandMemberManage(Model model, BandMemberDetailVO vo, Paging paging) {
-	    model.addAttribute("memberList", bandOptionService.bandOptionGetAllMemberList(vo, paging));
+	   // model.addAttribute("memberList", bandOptionService.bandOptionGetAllMemberList(vo, paging));
 		return "content/band2/bandMemberManage";
 	}
 
 	// 전체 회원 목록 페이징
 	@ResponseBody
-	@GetMapping("/bandGroup/GetAllMemberListAjax")
-	public List<BandMemberDetailVO> GetAllMemberListAjax(BandMemberDetailVO vo, Paging paging) {
-		return bandOptionService.bandOptionGetAllMemberList(vo, paging);
+	@PostMapping("/bandGroup/GetAllMemberList")
+	public List<BandMemberDetailVO> GetAllMemberList(@RequestBody BandMemberDetailVO vo) {
+		return bandOptionService.bandOptionGetAllMemberList(vo);
 	}
 
-	// 가입 대기 중 회원 목록
-	@GetMapping("/bandGroup/getWaitingMemberList")
+	// 가입 대기 중 회원 목록 페이징
 	@ResponseBody
-	public List<BandMemberDetailVO> GetWaitingMemberList(BandMemberDetailVO vo, Paging paging) {
-		return bandOptionService.bandOptionGetWaitingMemberList(vo, paging);
+	@PostMapping("/bandGroup/getWaitingMemberList")
+	public List<BandMemberDetailVO> GetWaitingMemberList(@RequestBody BandMemberDetailVO vo) {
+		return bandOptionService.bandOptionGetWaitingMemberList(vo);
 	}
 
 	// 강퇴된 회원 목록
-	@GetMapping("/bandGroup/getkickedMemberList")
 	@ResponseBody
-	public List<BandMemberDetailVO> GetkickedMemberList(BandMemberDetailVO vo, Paging paging) {
-		return bandOptionService.bandOptionGetkickedMemberList(vo, paging);
+	@PostMapping("/bandGroup/getkickedMemberList")
+	public List<BandMemberDetailVO> GetkickedMemberList(@RequestBody BandMemberDetailVO vo) {
+		System.out.println(vo);
+		return bandOptionService.bandOptionGetkickedMemberList(vo);
 	}
 	
 	// 멤버 수 카운팅
