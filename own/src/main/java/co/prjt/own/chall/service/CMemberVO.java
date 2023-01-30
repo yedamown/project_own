bd869eb7ce91ec4f2607634ea50106e71ae53c95
sujin <hsujin0819@gmail.com>
2023-01-30 17:54:30 +0900

    0130 sujin pm 5:55

--- own/src/main/java/co/prjt/own/chall/service/CMemberVO.java
+++ own/src/main/java/co/prjt/own/chall/service/CMemberVO.java
@@ -4,14 +4,22 @@ import lombok.Data;
 
 @Data
 public class CMemberVO {
+	String memNo;
 	String userId;
 	String memNickname;
-	int memAmt;
-	int memAcc;
-	String memBank;
 	String memIntro;
-	String memAccname;
-	String memNo;
 	
+	//현재 예치금
+	int memAmt;
+	//환불 계좌번호
+	int memAcc;
+	//환불 계좌예금주
+	String memAccname;
+	//환불계좌 은행
+	String memBank;
+
+	//프사 경로
 	String memImage;
+	//결제 충전한 금액
+	int price;
 }
--- own/src/main/java/co/prjt/own/chall/service/impl/CAmountServiceImpl.java
+++ own/src/main/java/co/prjt/own/chall/service/impl/CAmountServiceImpl.java
@@ -6,16 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import co.prjt.own.chall.mapper.CAmountMapper;
+import co.prjt.own.chall.mapper.CMemberMapper;
 import co.prjt.own.chall.service.CAmountService;
 import co.prjt.own.chall.service.CAmountVO;
+import co.prjt.own.chall.service.CMemberVO;
 
 @Component
 public class CAmountServiceImpl implements CAmountService{
 
 	@Autowired CAmountMapper mapper;
 	
+	//현재 예치금 같이 결제하기 위해.
+	@Autowired CMemberMapper member;
+	
 	@Override
-	public int insertAmount(CAmountVO vo) {
+	public int insertAmount(CAmountVO vo, CMemberVO mem) {
+		System.out.println(vo);
+		String type = vo.getAmtType();
+		System.out.println("============유형================" + type);
+		if(type.equals("출금") || type.equals("도전참가")) {
+			mem.setUserId(vo.getUserId());
+			mem.setPrice(vo.getPrice());
+			member.minusAmt(mem);
+		} else {
+			mem.setUserId(vo.getUserId());
+			mem.setPrice(vo.getPrice());
+			member.plusAmt(mem);
+		}
 		return mapper.insertAmount(vo);
 	}
 
