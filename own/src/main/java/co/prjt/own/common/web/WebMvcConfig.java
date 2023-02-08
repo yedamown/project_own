be7406dfd4d385cd28b56d33b3eb3d8b37380745
yedam <dodgo12@naver.com>
2023-02-08 10:19:41 +0900

    mergee

--- own/src/main/java/co/prjt/own/common/web/WebMvcConfig.java
+++ own/src/main/java/co/prjt/own/common/web/WebMvcConfig.java
@@ -9,6 +9,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/imgView/**")
-        .addResourceLocations("file:///C:test/");
+        .addResourceLocations("file:///C:/test/");
     }
 }
