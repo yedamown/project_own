55ed3ee32f6024be8edd86da35a2e3fe7a85419b
yedam <dodgo12@naver.com>
2023-02-10 17:52:01 +0900

    asffasd

--- own/src/main/java/co/prjt/own/security/WebSecurityConfig.java
+++ own/src/main/java/co/prjt/own/security/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig {
 		http.authorizeHttpRequests((requests) -> requests
 //				.antMatchers("/","/own/home","/css/**","/login","/own/sendmail").permitAll() // 홈은 모든사람 접근가능
 				.antMatchers("/","/own/login","/own/SigninForm","/own/singin/**","/own/idcheck","/own/admin/selectquest"
-						,"/own/admin/test","/own/mypage/**","/own/admin/myquestDelete").permitAll() // 특정권한만 가진사람만
+						,"/own/admin/test","/own/mypage/**","/own/admin/myquestDelete","/own/userInfo","/questionUpdate").permitAll() // 특정권한만 가진사람만
 				.antMatchers("/own/admin/**").hasRole("ADMIN") // 특정권한만 가진사람만
 				.antMatchers("/own/**").hasAnyRole("ADMIN","USER") // 특정권한만 가진사람만
 //				.anyRequest().authenticated() //이외에는 허가된사람만..
